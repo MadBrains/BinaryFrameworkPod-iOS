31ffb3a162e8a7f8f7cd32639375a3ae7d778d20
Alexander Khiger <alex10011997@gmail.com>
2022-04-14 17:54:55 +0400

    0.0.1

---
+++ BinaryFrameworkExample.xcframework/ios-arm64_x86_64-simulator/BinaryFrameworkExample.framework/Headers/BinaryFrameworkExample.h
@@ -0,0 +1,18 @@
+//
+//  BinaryFrameworkExample.h
+//  BinaryFrameworkExample
+//
+//  Created by Alexander Khiger on 13.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinaryFrameworkExample.
+FOUNDATION_EXPORT double BinaryFrameworkExampleVersionNumber;
+
+//! Project version string for BinaryFrameworkExample.
+FOUNDATION_EXPORT const unsigned char BinaryFrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinaryFrameworkExample/PublicHeader.h>
+
+
